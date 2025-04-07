de97e7e3c091f09ffc4f5d31c87ee1a7e5cf89c2
smjoy222 <smjoy222@gmail.com>
2025-04-08 05:44:21 +0600

    fix 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bazar_sadaai_app/screens/home_screen.dart';
-import 'package:bazar_sadaai_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,12 +8,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: HomeScreen(),
     );
   }
 }
